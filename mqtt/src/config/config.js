74b0d3d4a29171899c7f209528705f6eb7471a6d
Sergio Cortés <scortesprado@uc.cl>
2025-10-05 19:49:52 -0300

    feat(broker): added broker connections

---
+++ mqtt/src/config/config.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const MQTT_CONFIG = {
+  brokerUrl: `mqtt://${process.env.MQTT_HOST}:${process.env.MQTT_PORT}`,
+  options: {
+    clean: true,
+    connectTimeout: 4000,
+    clientId: process.env.MQTT_CLIENT_ID,
+    username: process.env.MQTT_USERNAME,
+    password: process.env.MQTT_PASSWORD,
+    reconnectPeriod: 1000,
+  },
+  channels: {
+    PROPERTIES_INFO: 'properties/info',
+    PROPERTIES_REQUESTS: 'properties/requests',
+    PROPERTIES_VALIDATION: 'properties/validation',
+  },
+};
+
+module.exports = MQTT_CONFIG;
