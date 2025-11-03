5d5f10ef935da20b40afc2a9c08318c1ca5bd71b
gaspar-mr05 <gaspar.magna@uc.cl>
2025-11-03 19:22:05 -0300

    add suggestion endpoint

--- api/src/index.js
+++ api/src/index.js
@@ -1,15 +1,17 @@
 const Router = require("@koa/router");
-const test = require('./routes/test');
-const properties = require('./routes/properties');
-const appointments = require('./routes/appointments');
-const wallet = require('./routes/wallet');
+const test = require("./routes/test");
+const properties = require("./routes/properties");
+const appointments = require("./routes/appointments");
+const suggestions = require("./routes/suggestions");
+const wallet = require("./routes/wallet");
 
 const router = new Router();
 
 // rutas
-router.use('/test', test.routes());
-router.use('/properties', properties.routes());
-router.use('/wallet', wallet.routes());
-router.use('/appointments', appointments.routes());
+router.use("/test", test.routes());
+router.use("/properties", properties.routes());
+router.use("/wallet", wallet.routes());
+router.use("/appointments", appointments.routes());
+router.use("/suggestions", suggestions.routes());
 
 module.exports = router;
---
+++ api/src/routes/suggestions.js
@@ -0,0 +1,56 @@
+const Router = require("@koa/router");
+const router = new Router();
+const { Property } = require("../models");
+
+router.get("/:property_id", async (ctx) => {
+  const { property_id } = ctx.params;
+
+  try {
+    const property = await Property.findByPk(property_id);
+
+    if (!property) {
+      ctx.status = 404;
+      ctx.body = { error: "Property not found" };
+      return;
+    }
+
+    // Armamos el cuerpo de la solicitud como el JobService espera
+    const propertyToSend = {
+      id: property.id,
+      name: property.name,
+      beedrooms: property.bedrooms,
+      price: property.price,
+    };
+
+    // Enviar POST al JobService
+    const response = await fetch(`${process.env.WORKERS_URL}/job`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ property: propertyToSend }),
+    });
+
+    const { job_id } = await response.json();
+
+    if (!job_id) {
+      ctx.throw(500, "No se recibió job_id del servicio externo");
+    }
+
+    // Esperar la respuesta final del job
+    const suggestionsResponse = await fetch(
+      `${process.env.WORKERS_URL}/job/${job_id}`,
+      { method: "GET", headers: { "Content-Type": "application/json" } }
+    );
+
+    const suggestionsData = await suggestionsResponse.json();
+
+    // Devolvemos la respuesta tal cual llega del servicio
+    ctx.status = 200;
+    ctx.body = suggestionsData;
+  } catch (error) {
+    console.error("❌ Error al obtener recomendaciones:", error);
+    ctx.status = 500;
+    ctx.body = { error: error.message };
+  }
+});
+
+module.exports = router;
