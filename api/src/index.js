25d34a008c7b35fd27873503bb94930c9e04b348
gaspar-mr05 <gaspar.magna@uc.cl>
2025-11-26 20:29:38 -0300

    feat: add admin route and middleware

--- api/src/index.js
+++ api/src/index.js
@@ -4,7 +4,7 @@ const properties = require("./routes/properties");
 const appointments = require("./routes/appointments");
 const wallet = require("./routes/wallet");
 const suggestions = require("./routes/suggestions");
-
+const admin = require("./routes/admin");
 const router = new Router();
 
 // rutas
@@ -13,5 +13,6 @@ router.use("/properties", properties.routes());
 router.use("/wallet", wallet.routes());
 router.use("/appointments", appointments.routes());
 router.use("/suggestions", suggestions.routes());
+router.use("/admin", admin.routes());
 
 module.exports = router;
