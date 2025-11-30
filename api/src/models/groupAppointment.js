98f557926ca0d27626f387354ac8913230c1c74a
gaspar-mr05 <gaspar.magna@uc.cl>
2025-11-29 23:56:45 -0300

    feat: add group appointment model

---
+++ api/src/models/groupAppointment.js
@@ -0,0 +1,30 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class GroupAppointment extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  GroupAppointment.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+      },
+      property_id: DataTypes.NUMBER,
+      quantity: DataTypes.NUMBER,
+      discount: DataTypes.NUMBER,
+    },
+    {
+      sequelize,
+      modelName: "GroupAppointment",
+    }
+  );
+  return GroupAppointment;
+};
